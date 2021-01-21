7f82bfc30d1832b5a13ef81501009d72821de7fe
FdI96 <federicolopezce@gmail.com>
2021-01-21 14:47:03 -0300

    fixing linters

--- enumerablefile.rb
+++ enumerablefile.rb
@@ -5,13 +5,17 @@ module Enumerable
       yield self[i]
     end
   end
+  
   #my_each_with_index
+
   def my_each_with_index
     self.length.times do |i|
       yield self[i], i
     end
   end
+
   #my_select
+
   def my_select
     result = []
     self.my_each do |elem|
@@ -21,7 +25,9 @@ module Enumerable
     end
     p result
   end
+
   #my_all
+
   def my_all
     self.my_each do |elem|
       unless yield elem
@@ -30,7 +36,9 @@ module Enumerable
     end
     return true
   end
+
   #my_any
+
   def my_any
     self.my_each do |elem|
       if yield elem
@@ -39,7 +47,9 @@ module Enumerable
     end
     return false
   end
+
   #my_one
+
   def my_one
     self.my_each do |elem|
       if yield elem
@@ -48,7 +58,9 @@ module Enumerable
     end
     return false
   end
+
   #my_none
+
   def my_none
     self.my_each do |elem|
       if yield elem
@@ -57,7 +69,9 @@ module Enumerable
     end
     return true
   end
+
   #my_count
+
   def my_count(search = nil)
     if search == nil
       return self.length
@@ -71,7 +85,9 @@ module Enumerable
       return count 
     end
   end
+
   #my_map
+
   def my_map
     result = []
     self.my_each do |elem|
@@ -79,15 +95,19 @@ module Enumerable
     end
     return result
   end
+
   #my_inject or reduce
+
   def my_inject(acc = 0)
     self.my_each do |elem|
       acc = yield elem, acc
     end
     return acc
   end
+
   #multiply_els
+
   def multiply_els
-    self.my_inject(1) {|acc, elem| acc * elem}
+    self.my_inject(1) { |acc, elem| acc * elem }
   end
 end
